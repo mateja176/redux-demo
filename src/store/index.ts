86fc63e21ef92e8e071b69968673fc8fe67a40ce
mateja <mateja176@gmail.com>
2019-04-10 14:14:25 +0200

    Removes env types

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,11 @@
 import { applyMiddleware, createStore, Store } from "redux"
 import { composeWithDevTools } from "redux-devtools-extension"
 import logger from "redux-logger"
-import { EnvironmentTypes } from "../models/environment"
 import reducer, { State } from "./reducer"
 
 export const middleware = [logger]
 
-const configureStore = (env: EnvironmentTypes): Store<State> => {
+const configureStore = (env: string): Store<State> => {
   switch (env) {
     case "production":
       return createStore(reducer)
